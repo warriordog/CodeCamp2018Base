2404a9e19f1c0876699a93a1d33b849a216d570f
chris <acomputerdog@gmail.com>
2018-07-18 10:55:06 -0400

    Switch to intern team

--- Bootstrap.js
+++ Bootstrap.js
@@ -31,7 +31,7 @@ var BootstrapData =
     startAtBeginningOfMazeList: false,
 
     // set at the repository level, shouldn't be changed by teams
-    teamName: "Team Charlie",
+    teamName: "Intern Invasion",
     mazes: [
         "3:3:TinyTim",
         "10:10:SlipperyDevil",
--- Bots/CodeCamp-Bot1.js
+++ Bots/CodeCamp-Bot1.js
@@ -1,5 +1,31 @@
 'use strict';
 
+const MAX_DIST 3
+const DIST_UNKNOWN = -2;
+const DIST_SAFE = -1;
+const DIST_HERE = 0;
+
+const OBSERVED_NO = 0;
+const OBSERVED_LOOKED = 1;
+const OBSERVED_VISITED = 2;
+
+const RATING_IMPOSSIBLE = -1.0;
+const RATING_WORST = 0.0;
+const RATING_BAD = 0.25;
+const RATING_NEUTRAL = 0.5;
+const RATING_GOOD = 0.75;
+const RATING_BEST = 1.0;
+
+const CONF_WORST = 0.0;
+const CONF_BAD = 0.25;
+const CONF_NEUTRAL = 0.5;
+const CONF_GOOD = 0.75;
+const CONF_BEST = 1.0;
+
+const WALL_UKNOWN = -1;
+const WALL_PRESENT = 0;
+const WALL_ABSENT = 1;
+
 var command = {
     action: null,
     direction: null,
@@ -7,9 +33,41 @@ var command = {
 };
 
 var imFacing = "north";
-var lookedRight = false;
-var lookedLeft = false;
 
+// 2D array of cells
+var maze = null;
+
+var mazeWidth = 0;
+var mazeHeight = 0;
+
+/*
+var maze = new Array(10);
+for (var x = 0; x < maze.length; x++) {
+    maze[x] = new Array(10);
+    for (var y = 0; y < maze[x].length; y++) {
+        maze[x][y] = {
+            x: x,
+            y: y,
+
+            observed: OBSERVED_NO,
+
+            fire: DIST_UNKNOWN,
+            pit: DIST_UNKNOWN,
+            exit: DIST_UNKNOWN,
+            start: DIST_UNKNOWN,
+            
+            walls: {
+                north: WALL_UKNOWN,
+                east: WALL_UKNOWN,
+                south: WALL_UKNOWN,
+                west: WALL_UKNOWN
+            }
+        };
+    }
+}
+*/
+
+/*
 function openExit(direction, sight) {
     var exits = getExits(sight);
 
@@ -28,7 +86,9 @@ function openExit(direction, sight) {
 
     return false;
 }
+*/
 
+/*
 function getExits(sight) {
     var exits = {
         north: false,
@@ -54,6 +114,7 @@ function getExits(sight) {
 
     return exits;
 }
+*/
 
 function getRight(direction) {
     if ("north" == direction) {
@@ -87,6 +148,300 @@ function getLeft(direction) {
     return "south"; // don't know!   
 }
 
+function isInBounds(x, y) {
+    return x >= 0 && x < 10 && y >= 0 && y < 10;
+}
+
+function getOffsetX(dir) {
+    /*
+    if (dir == 'north') {
+        return 0;
+    }
+    if (dir == 'south') {
+        return 0;
+    }
+    */
+    if (dir == 'east') {
+        return 1;
+    }
+    if (dir == 'west') {
+        return -1;
+    }
+
+    return 0;
+}
+
+function getOffsetY(dir) {
+    if (dir == 'north') {
+        return -1;
+    }
+    if (dir == 'south') {
+        return 1;
+    }
+
+    return 0;
+}
+
+function getCellbyDir(x, y, dir) {
+    var nX = x + getOffsetX(dir);
+    var nY = y + getOffsetY(dir);
+
+    if (isInBounds(nX, nY)) {
+        return maze[nX][nY];
+    } else {
+        return null;
+    }
+}
+
+function invertDir(dir) {
+    if (dir == 'north') {
+        return 'south';
+    }
+    if (dir == 'south') {
+        return 'north';
+    }
+    if (dir == 'east') {
+        return 'west';
+    }
+    if (dir == 'west') {
+        return 'east';
+    }
+    return null;
+}
+
+function addWall(x, y, dir) {
+    var cell = maze[x][y];
+    cell.walls[dir] = WALL_PRESENT;
+
+    var neighbor = getCellByDir(x, y, dir);
+    if (neighbor != null) {
+        neighbor.walls[invertDir(dir)] = WALL_PRESENT;
+    }
+}
+
+function canMoveInDir(x, y, dir) {
+    var cell = maze[x][y];
+    
+    // check for walls
+    if (cell.walls[dir] != WALL_PRESENT) {
+        return false;
+    }
+
+    // check bounds
+    return getCellbyDir(x, y, dir) != null;
+}
+
+function shouldMoveInDir(x, y, dir) {
+    if (canMoveInDir(x, y, dir)) {
+        var next = getCellbyDir(x, y, dir);
+        if (next.fire == DIST_HERE) {
+            return false;
+        }
+        if (next.pit == DIST_HERE) {
+            return false;
+        }
+    }
+}
+
+function createZeroRating() {
+    return {
+        rating: RATING_IMPOSSIBLE,
+        confidence: CONF_BEST
+    };
+}
+
+
+/*
+ * Rating and confidence calculationas
+ */
+ 
+ // fire
+function calcFireRate(fire) {
+    if (fire == DIST_UNKNOWN) {
+        return RATING_NEUTRAL;
+    }
+    if (fire == DIST_HERE) {
+        return RATING_WORST;
+    }
+    if (fire == DIST_SAFE) {
+        return RATING_BEST;
+    }
+    return Math.min(RATING_GOOD, (fire / (MAX_DIST + 1)) * RATING_GOOD);
+}
+function calcFireConf(fire) {
+    if (fire == DIST_UNKNOWN) {
+        return CONF_WORST;
+    }
+    if (fire == DIST_HERE) {
+        return CONF_BEST;
+    }
+    if (fire == DIST_SAFE) {
+        return CONF_BEST;
+    }
+    return Math.min(CONF_GOOD, (fire / (MAX_DIST + 1)) * CONF_GOOD);
+}
+
+//pits
+function calcPitRate(pit) {
+    if (pit == DIST_UNKNOWN) {
+        return RATING_NEUTRAL;
+    }
+    if (pit == DIST_HERE) {
+        return RATING_WORST;
+    }
+    if (pit == DIST_SAFE) {
+        return RATING_BEST;
+    }
+    return Math.min(RATING_GOOD, (pit / (MAX_DIST + 1)) * RATING_GOOD);
+}
+function calcPitConf(pit) {
+    if (pit == DIST_UNKNOWN) {
+        return CONF_WORST;
+    }
+    if (pit == DIST_HERE) {
+        return CONF_BEST;
+    }
+    if (pit == DIST_SAFE) {
+        return CONF_BEST;
+    }
+    return Math.min(CONF_GOOD, (pit / (MAX_DIST + 1)) * CONF_GOOD);
+}
+
+// visited
+function calcVisitedRate(visited) {
+    if (visited == OBSERVED_VISITED) {
+        return RATING_WORST;
+    }
+    if (visited == OBSERVED_LOOKED) {
+        return RATING_NEUTRAL
+    }
+    return RATING_BEST;
+}
+function calcVisitedConf(visited) {
+    return CONF_BEST;
+}
+
+// walls
+function rateWall(wall) {
+    if (wall == WALL_PRESENT) {
+        return -0.25;
+    }
+    if (wall == WALL_ABSENT) {
+        return 0.25;
+    }
+    return 0.0;
+}
+function calcWallsRate(walls) {
+    var rate = RATING_NEUTRAL;
+    rate += rateWall(walls.north);
+    rate += rateWall(walls.south);
+    rate += rateWall(walls.east);
+    rate += rateWall(walls.west);
+    
+    return rate;
+}
+function calcWallsConf(walls) {
+    var conf = CONF_WORST;
+    if (walls.north != WALL_UNKNOWN) {
+        conf += 0.25;
+    }
+    if (walls.south != WALL_UNKNOWN) {
+        conf += 0.25;
+    }
+    if (walls.east != WALL_UNKNOWN) {
+        conf += 0.25;
+    }
+    if (walls.west != WALL_UNKNOWN) {
+        conf += 0.25;
+    }
+    
+    return conf;
+}
+
+/*
+ * Cell rating functions
+ */
+function getCellRating(cell) {
+    var confidence = 0.0;
+    var rating = 0.0;
+
+    // value += weight * normalized value
+
+    rating += 0.2 * calcFireRate(cell.fire);
+    confidence += 0.2 * calcFireConf(cell.fire);
+
+    rating += 0.2 * calcPitRate(cell.pit);
+    confidence += 0.2 * calcPitConf(cell.fire);
+
+    rating += 0.5 * calcVisitedRate(cell.visited);
+    confidence += 0.5 * calcVisitedConf(cell.visited);
+
+    rating += 0.1 * calcWallsRate(cell.walls);
+    confidence += 0.1 * calcWallsConf(cell.walls);
+
+    return {
+        confidence: confidence,
+        rating: rating
+    }
+}
+
+function getCellRating(x, y) {
+    if (isInBounds(x, y)) {
+        return getCellRating(maze[x][y]);
+    } else {
+        return createZeroRating();
+    }
+}
+
+function getCellRating(x, y, dir) {
+    var cell = getCellByDir(x, y, dir);
+    if (cell != null) {
+        return createZeroRating();
+    }
+}
+
+/*
+ * Parsing functions
+ */
+function initMaze(gameState) {
+    var mazeDims = gameState.mazeId.split(":");
+    var mazeX = parseInt(mazeDims[0]);
+    var mazeY = parseInt(mazeDims[1]);
+    
+    mazeWidth = mazeX;
+    mazeHeight = mazeY;
+    
+    maze = new Array(mazeWidth);
+    for (var x = 0; x < mazeWidth; x++) {
+        maze[x] = new Array(mazeHeight);
+        for (var y = 0; y < mazeHeight; y++) {
+            maze[x][y] = {
+                x: x,
+                y: y,
+
+                observed: OBSERVED_NO,
+
+                fire: DIST_UNKNOWN,
+                pit: DIST_UNKNOWN,
+                exit: DIST_UNKNOWN,
+                start: DIST_UNKNOWN,
+                
+                walls: {
+                    north: WALL_UKNOWN,
+                    east: WALL_UKNOWN,
+                    south: WALL_UKNOWN,
+                    west: WALL_UKNOWN
+                }
+            };
+        }
+    }
+}
+
+// Parses an engram and updates the maze data
+function parseEngram(engram) {
+    updateWalls()
+}
+
 module.exports = {
     /**
      * @param {Object} gameState
@@ -97,10 +452,20 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
+        // If we are just starting the maze, then look
         if (null == gameState) {
             command.action='look';
             command.direction=imFacing;
         } else {
+            // if this is first action, then build maze
+            if (maze == null) {
+                initMaze(gameState);
+            }
+            
+            // parse the engram and update maze
+            parseEngram(gameState.engram)    
+            
+        /*
                 // let's see what we saw
                 if (!gameState.engram.sight.toLowerCase().includes('lava') &&
                     !gameState.engram.sight.toLowerCase().includes('wall')) {
@@ -146,6 +511,7 @@ module.exports = {
                         imFacing = command.direction;
                     }
                 }
+                */
             }
         
         return command;
